0bea19300673efc0bafc21318b7775a7cfc7b4e6
Lars Kluge <l@larskluge.com>
2015-08-13 16:58:33 +0300

    Fix secret config

--- lib/artgun/config.rb
+++ lib/artgun/config.rb
@@ -1,12 +1,12 @@
 module Artgun
 
   @api_key       = ENV['ARTGUN_API_KEY']
-  @api_signature = ENV['ARTGUN_API_SIGNATURE']
+  @api_secret    = ENV['ARTGUN_API_SECRET']
   @api_endpoint  = ENV['ARTGUN_API_ENDPOINT']
   @test_mode     = !!(ENV['ARTGUN_TEST_MODE'].to_s =~ /\A(true|t|yes|y|1)\z/i)
 
   class << self
-    attr_accessor :api_key, :api_signature, :api_endpoint, :test_mode
+    attr_accessor :api_key, :api_secret, :api_endpoint, :test_mode
     alias :test_mode? :test_mode
   end
 
